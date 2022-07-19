afc7df118344927ab35e44e566f65967b818b096
idesmar <idesmar@gmail.com>
2022-07-19 20:22:05 +0800

    day06 added QNA note

--- day06/src/components/Level1.js
+++ day06/src/components/Level1.js
@@ -24,6 +24,7 @@ const Level1 = () => {
   return (
     <section>
       <h2>Exercise Level 1</h2>
+      <small style={{display: 'block', textAlign: 'center', marginBottom: '0.5rem'}}><em style={{fontWeight: '500'}}>NOTE:</em> All answers are based on the author's understanding. It is greatly appreciated if any needed corrections or clarifications is brought to the attention of the author. Fastest way to connect is twitter <a style={{backgroundColor: 'green', color: '#fff', paddingInline: '0.6rem', borderRadius: '100vw'}} href="https://twitter.com/idesmar">@idesmar</a>. Thank you!</small>
       <QNAs />
     </section>
   )
