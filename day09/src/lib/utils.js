9939346f577a5091681ba6f3e7121071a2aa0336
idesmar <idesmar@gmail.com>
2022-08-02 14:12:37 +0800

    fix + new func

--- day09/src/lib/utils.js
+++ day09/src/lib/utils.js
@@ -144,8 +144,9 @@ const isContrastPassing = (fColor, bgColor, AAA_Level = false, Large_18ptAbove =
 }
 
 
-const colorMe = (fColor = 'light') => {
-  const _fColor = 'dark' ? '#000000' : 'light' ? '#ffffff' : fColor
+const colorMe = (theme = 'light') => {
+  const _fColor = theme === 'light' ? '#000000' : theme === 'dark' ? '#ffffff' : theme
+  // TODO: checker if color is in hex
 
   const colorArr = []
   const getColor = () => {
@@ -164,9 +165,36 @@ const rem = (px = 0) => {
   return +px / 16 + 'rem'
 }
 
+/*
+multiple px (up to 4) to rem conversion
+  format based on top right bottom left (ie. padding, margin)
+  @function rem4($px1, $px2: $px1, $px3: $px1, $px4: $px2) {
+    @return rem($px1) rem($px2) rem($px3) rem($px4);
+  }
+*/
+const rem4 = (px1 = 0, px2 = px1, px3 = px1, px4 = px2) => {
+  if (isNaN(px1)) {
+    console.error('invalid rem4 input')
+    return 0
+  }
+  return `${rem(px1)} ${rem(px2)} ${rem(px3)} ${rem(px4)}`
+}
+
+const em = (px = 0) => {
+  if (isNaN(px)) return px
+  return +px / 16 + 'em'
+}
+
 const deRem = (rem = '') => {
   const lastDigit = rem.length - 3
   return +rem.slice(0, lastDigit) * 16
+}
+
+const toTitleCase = (str = '') => {
+  return (
+    str.split(' ')
+      .map(subStr => (subStr[0].toUpperCase() + subStr.slice(1)))
+  )
 }
 
 
@@ -174,5 +202,6 @@ export {
   seqNumsArr,
   isPrime, isPrime0, isEven, isOdd,
   hexColor, isDark, isContrastPassing, colorMe,
-  rem, deRem
+  rem, rem4, deRem, em,
+  toTitleCase,
 }
