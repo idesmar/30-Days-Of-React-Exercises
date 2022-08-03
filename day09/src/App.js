e525375aa58800d35dd55b17b65545f1850573ec
idesmar <idesmar@gmail.com>
2022-08-03 19:50:58 +0800

    day09 lev2.2 restructure

--- day09/src/App.js
+++ day09/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { headingStyle } from './components/globalStyle'
+import { headingStyle } from './components/__globalStyle'
 import Level1Class from './components/Level1Class'
 import Level2Class from './components/Level2Class'
+import Level3Class from './components/Level3Class'
 import level1qna from './data/level1qna'
 import './App.css'
 import { rem } from './lib/utils'
@@ -12,6 +13,7 @@ import fallBg from './assets/undraw_autumn_re_rwy0.svg'
 
 const sectionLimit = {
   width: rem(600),
+  maxWidth: '100%',
   marginInline: 'auto',
 }
 
@@ -27,77 +29,155 @@ class App extends Component {
     {
       name: 'winter',
       months: [12, 1, 2],
-      background: winterBg,
+      backgroundImg: winterBg,
     },
     {
       name: 'spring',
       months: [3, 4, 5],
-      background: springBg,
+      backgroundImg: springBg,
     },
     {
       name: 'summer',
       months: [6, 7, 8],
-      background: summerBg,
+      backgroundImg: summerBg,
     },
     {
       name: 'fall',
       months: [9, 10, 11],
-      background: fallBg,
+      backgroundImg: fallBg,
     },
   ]
 
-  getCurrentSeasonIdx = () => {
-    const date = new Date()
-    const month = date.getMonth() + 1
+  timeOfDays = [
+    {
+      name: 'morning',
+      minHour: 6,
+      backgroundClr: '#dfffda',
+    },
+    {
+      name: 'noon',
+      minHour: 12,
+      backgroundClr: '#fce684',
+    },
+    {
+      name: 'evening',
+      minHour: 18,
+      backgroundClr: '#093263',
+    },
+    {
+      name: 'night',
+      minHour: 0,
+      backgroundClr: '#09011f',
+    },
+  ]
 
+  getCurrentSeason = () => {
+    const month = new Date().getMonth() + 1
     const seasonIdx = this.seasons.findIndex(season => season.months.includes(month))
-    return seasonIdx
+    return this.seasons[seasonIdx].name
   }
+  currentSeason = this.getCurrentSeason()
+
+
+  getCurrentTimeOfDay = () => {
+    const hour = new Date().getHours()
+    const todIdx = this.timeOfDays.findIndex(tod => {
+      const { minHour } = tod
+      const maxHour = minHour + 5
+      return ( hour >= minHour && hour <= maxHour )
+    })
+    return this.timeOfDays[todIdx].name
+  }
+  currentTimeOfDay = this.getCurrentTimeOfDay()
 
   state = {
     userInputSeason: false,
-    season: this.seasons[this.getCurrentSeasonIdx()].name,
+    season: this.currentSeason,
+
+    userInputTimeOfDay: false,
+    timeOfDay: this.currentTimeOfDay,
   }
 
   changeUserInputSeason = () => {
     this.setState({userInputSeason: !this.state.userInputSeason})
   }
 
+  changeUserInputTimeOfDay = () => {
+    this.setState({userInputTimeOfDay: !this.state.userInputTimeOfDay})
+  }
+
   changeSeason = (e) => {
-    const newSeason = e.target.value
-    this.setState({ season: newSeason })
+    this.setState({ season: e.target.value })
+  }
+
+  changeTimeOfDay = (e) => {
+    this.setState({ timeOfDay: e.target.value })
   }
 
   render() {
     const qna = [...level1qna]
-    const { userInputSeason, season } = this.state
+    const {
+      userInputSeason, season,
+      userInputTimeOfDay, timeOfDay
+    } = this.state
 
-    const seasons = this.seasons
+    const seasons = [...this.seasons]
     const seasonIdx = seasons.findIndex(season => season.name === this.state.season)
+    const backgroundImg = seasons[seasonIdx].backgroundImg
 
-    const backgroundImg = seasons[seasonIdx].background
+    const timeOfDays = [...this.timeOfDays]
+    const todIdx = timeOfDays.findIndex(tod => tod.name === this.state.timeOfDay)
+    const backgroundClr = timeOfDays[todIdx].backgroundClr
+
+    const fColor = (timeOfDay === 'night' || timeOfDay === 'evening')
+                    ? '#ffffff' : '#000000'
+
     const appStyle = {
       backgroundImage: `url(${backgroundImg})`,
+      backgroundColor: backgroundClr,
       backgroundRepeat: 'no-repeat',
-      backgroundSize: 'contain',
+      // backgroundSize: 'contain',
       backgroundPosition: 'center',
+      color: fColor,
       minHeight: '100vh',
+      padding: '1rem',
     }
 
     const seasonNames = seasons.map(season => season.name)
-    const currentSeason = seasonNames[this.getCurrentSeasonIdx()]
+    const currentSeason = this.currentSeason
+    const timeOfDayNames = timeOfDays.map(tod => tod.name)
+    const currentTimeOfDay = this.currentTimeOfDay
+
     return (
       <div style={appStyle}>
         <Header />
-        <Level1Class style={sectionLimit} qna={qna} />
+        <Level1Class
+          style={sectionLimit}
+          qna={qna}
+        />
+
+
         <Level2Class
           style={sectionLimit}
+
           seasons={seasonNames}
           currentSeason={currentSeason}
           existingSeason={season}
           userInputSeason={userInputSeason}
           handleSeasonInput={this.changeUserInputSeason}
           handleSeasonChange={this.changeSeason}
+
+          timeOfDays={timeOfDayNames}
+          currentTimeOfDay={currentTimeOfDay}
+          existingTimeOfDay={timeOfDay}
+          userInputTimeOfDay={userInputTimeOfDay}
+          handleTimeOfDayInput={this.changeUserInputTimeOfDay}
+          handleTimeOfDayChange={this.changeTimeOfDay}
+        />
+
+
+        <Level3Class
+          style={sectionLimit}
         />
       </div>
     )
--- day09/src/components/Level2Class.js
+++ day09/src/components/Level2Class.js
@@ -1,9 +1,11 @@
 import { Component } from 'react'
 import { rem, rem4, em, toTitleCase } from '../lib/utils'
-import { headingStyle } from './globalStyle'
+import { headingStyle } from './__globalStyle'
 
 /*
   Make a single page application which changes the body of the background based on the season of the year(Autumn, Winter, Spring, Summer)
+
+  Make a single page application which change the body of the background based on the time of the day(Morning, Noon, Evening, Night)
 */
 
 /* ////////////////////////////////////////////////////////
@@ -34,61 +36,126 @@ const Button = ({ text, onClick, style }) => {
 }
 
 
-/* ////////////////////////////////////////////////////////
-  FUNCTIONAL COMPONENT
-//////////////////////////////////////////////////////// */
+const Input = ({
+  input,
+  groupName,
+  existing,
+  onChange,
+}) => {
 
-const Season = ({ season, existingSeason, onChange }) => {
-
-  const radioStyle = {
-    all: 'revert',
-  }
-
-  const labelStyle = {
-    cursor: 'pointer',
-  }
+  const radioStyle = { all: 'revert', }
+  const labelStyle = {  cursor: 'pointer', }
 
   return (
     <>
       <input
         style={radioStyle}
         type="radio"
-        name='season'
-        value={season}
-        id={season}
+        name={groupName}
+        value={input}
+        id={input}
         onChange={onChange}
-        checked={existingSeason === season}
+        checked={existing === input}
       />
       <label
         style={labelStyle}
-        htmlFor={season}
+        htmlFor={input}
       >
-        {toTitleCase(season)}
+        {toTitleCase(input)}
       </label>
     </>
   )
 }
 
-class SeasonsClass extends Component {
+const Fieldset = ({
+  content,
+  inputs,
+  groupName,
+  existing,
+  onChange,
+  style,
+}) => {
 
-  render() {
-    const { seasons, onChange, existingSeason } = this.props
+  const defaultFieldsetStyle = { all: 'revert' }
+  const fieldsetStyle = { ...defaultFieldsetStyle, ...style }
 
-    return (
-      <fieldset style={{all: 'revert'}}>
-        <legend>Choose the season</legend>
-        {seasons.map(season => (
-          <Season
-            key={season}
-            season={season}
-            existingSeason={existingSeason}
+  return (
+    <fieldset style={fieldsetStyle}>
+      <legend>
+        {content}
+      </legend>
+      {
+        inputs.map(input => (
+          <Input
+            key={input}
+            input={input}
+            groupName={groupName}
+            existing={existing}
             onChange={onChange}
           />
-        ))}
-      </fieldset>
-    )
-  }
+        ))
+      }
+    </fieldset>
+  )
 }
+
+
+/* ////////////////////////////////////////////////////////
+  FUNCTIONAL COMPONENT
+//////////////////////////////////////////////////////// */
+
+// const Season = ({ season, existingSeason, onChange }) => {
+
+//   const radioStyle = {
+//     all: 'revert',
+//   }
+
+//   const labelStyle = {
+//     cursor: 'pointer',
+//   }
+
+//   return (
+//     <>
+//       <input
+//         style={radioStyle}
+//         type="radio"
+//         name='season'
+//         value={season}
+//         id={season}
+//         onChange={onChange}
+//         checked={existingSeason === season}
+//       />
+//       <label
+//         style={labelStyle}
+//         htmlFor={season}
+//       >
+//         {toTitleCase(season)}
+//       </label>
+//     </>
+//   )
+// }
+
+
+// class SeasonsClass extends Component {
+
+//   render() {
+//     const { seasons, onChange, existingSeason } = this.props
+
+//     return (
+//       <fieldset style={{all: 'revert'}}>
+//         <legend>Choose the season</legend>
+//         {seasons.map(season => (
+//           <Season
+//             key={season}
+//             season={season}
+//             existingSeason={existingSeason}
+//             onChange={onChange}
+//           />
+//         ))}
+//       </fieldset>
+//     )
+//   }
+// }
 
 
 /* ////////////////////////////////////////////////////////
@@ -99,37 +166,92 @@ class Level2Class extends Component {
 
   render() {
     const {
-      style, seasons, currentSeason, existingSeason,
+      style,
+      // * Level2.1 Seasons
+      seasons, currentSeason, existingSeason,
       userInputSeason, handleSeasonInput,
-      handleSeasonChange
+      handleSeasonChange,
+      // * Level2.2 Time of Day
+      timeOfDays, currentTimeOfDay, existingTimeOfDay,
+      userInputTimeOfDay, handleTimeOfDayInput,
+      handleTimeOfDayChange,
     } = this.props
 
-    const text = userInputSeason ? `Current season: ${toTitleCase(currentSeason)}` : 'Choose a season?'
-    const seasonInput = userInputSeason && (
-      <SeasonsClass
-        seasons={seasons}
-        existingSeason={existingSeason}
+
+    const textSeason = userInputSeason
+      ? `Current season: ${toTitleCase(currentSeason)}`
+      : 'Choose a season?'
+
+    const textTimeOfDay = userInputTimeOfDay
+      ? `Current time of day: ${toTitleCase(currentTimeOfDay)}`
+      : 'Choose a time of day?'
+
+    // content,
+    // inputs,
+    // groupName,
+    // existing,
+    // onChange,
+    // style,
+    const seasonFieldset = userInputSeason && (
+      <Fieldset
+        inputs={seasons}
+        content={textSeason}
+        existing={existingSeason}
+        groupName={'seasons'}
         onChange={handleSeasonChange}
       />
     )
 
-    const backgroundColor = userInputSeason ? 'red' : 'green'
-    const buttonStyle = {
+    const timeOfDayInput = userInputTimeOfDay && (
+      <Fieldset
+        inputs={timeOfDays}
+        content={textTimeOfDay}
+        existing={existingTimeOfDay}
+        groupName={'timeOfDay'}
+        onChange={handleTimeOfDayChange}
+      />
+    )
+
+    const defaultButtonStyle = {
       minWidth: 'fit-content',
       width: rem(200),
       maxWidth: '100%',
-      backgroundColor,
+      display: 'block',
+      margin: '0 auto',
+    }
+
+    const buttonSeasonStyle = {
+      ...defaultButtonStyle,
+      backgroundColor: userInputSeason ? 'red' : 'green',
+    }
+    const buttonTimeOfDayStyle = {
+      ...defaultButtonStyle,
+      backgroundColor: userInputTimeOfDay ? 'red' : 'green',
     }
 
     return (
       <section style={style} >
         <h2 style={headingStyle}>Level 2</h2>
-        <Button
-          text={text}
-          onClick={handleSeasonInput}
-          style={buttonStyle}
-        />
-        { seasonInput }
+
+        <div>
+          <h3 style={headingStyle}>Level 2.1</h3>
+          <Button
+            text={textSeason}
+            onClick={handleSeasonInput}
+            style={buttonSeasonStyle}
+          />
+          {seasonFieldset}
+        </div>
+
+        <div>
+          <h3 style={headingStyle}>Level 2.2</h3>
+          <Button
+            text={textTimeOfDay}
+            onClick={handleTimeOfDayInput}
+            style={buttonTimeOfDayStyle} // TODO: change later ?
+          />
+          {timeOfDayInput}
+        </div>
       </section>
     )
   }
