8eaba8110c619b7c96b495672d0e8f255eb20aaa
idesmar <idesmar@gmail.com>
2022-08-08 21:23:25 +0800

    init day11

---
+++ day11/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <>
+        App placeholder
+      </>
+    )
+  }
+}
+
+export default App
