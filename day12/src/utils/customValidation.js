b728a66a71046224b06de589a58f346044221ef5
idesmar <idesmar@gmail.com>
2022-08-23 16:45:42 +0800

    added custom validation to utils

---
+++ day12/src/utils/customValidation.js
@@ -0,0 +1,26 @@
+const isNameFormatValid = (name) => {
+  // source: https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+  const nameRegEx = /^[^0-9_!¡?÷?¿/\\+=@#$%^&*(){}|~<>;:[\]]{2,}$/ // ! test more
+  return nameRegEx.test(name)
+}
+
+const isEmailValid = (email = '') => {
+  const emailRegEx = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/
+  return emailRegEx.test(email)
+}
+
+const isUsernameValid = (username = '') => {
+  const usernameRegEx = /\w+/
+  return usernameRegEx.test(username) && username.length >= 6
+}
+
+const isPasswordMatching = (password = '', password2 = '') => {
+  return password && password2 && password === password2
+}
+
+export {
+  isNameFormatValid,
+  isEmailValid,
+  isUsernameValid,
+  isPasswordMatching,
+}
