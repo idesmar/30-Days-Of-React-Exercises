cab34afa7fc90d1afef968f60144bc8679fd6ec5
idesmar <idesmar@gmail.com>
2022-09-23 18:16:01 +0800

    day16 added h1

--- day16/src/App.js
+++ day16/src/App.js
@@ -6,9 +6,11 @@ import css from './app.module.css'
 
 const { app } = css
 
+const Heading1 = () => <h1>30 Days of React: Day 16 | Higher Order Component</h1>
 const App = () => {
   return (
     <div className={app}>
+      <Heading1 />
       <Level1 />
       <Sample1 />
       <Sample2 />
