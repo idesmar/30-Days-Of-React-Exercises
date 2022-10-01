7892239634d80d3d3450f1f73b71546e142ff50f
idesmar <idesmar@gmail.com>
2022-10-02 07:52:52 +0800

    day17 ./components -> ./pages

--- day17/src/routes/NavRoutes.js
+++ day17/src/routes/NavRoutes.js
@@ -1,32 +1,50 @@
 import { Routes, Route } from "react-router-dom"
-import { About } from "../components/About"
-import { Home } from "../components/Home"
-import { Contact, SomeContact, SomeOtherContact } from "../components/Contact"
-import { NotFound } from "../components/NotFound"
+import { About } from "../pages/About"
+import { Home } from "../pages/Home"
+import { NotFound } from "../pages/NotFound"
+import {
+  Contact,
+  SomeContact,
+  SomeOtherContact,
+} from "../pages/Contact"
 import {
   Challenges,
   Challenge1,
   Challenge2,
   OtherChallenges,
-} from "../components/Challenges"
+} from "../pages/Challenges"
+import { ChallengesLayout } from "../layouts/ChallengesLayout"
+import { SharedLayout } from "../layouts/SharedLayout"
+
 
 const NavRoutes = () => {
   /* Returning Navigation Nested Route */
   return (
     <Routes>
       <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
 
-        {/* Nested route for challenges with dynamic route :id */}
-        <Route path="challenges">
+        <Route index element={<Home />} />
+
+        {/* //> Nested route for challenges with dynamic route :id */}
+        {/* //> Layout that shows an aside > nav is passed as an element
+          that allows the layout element to show on every page that matches the Route path */}
+        <Route path="challenges" element={<ChallengesLayout />}>
           <Route index element={<Challenges />} />
+          {/* //? Routes below have more priority than Dynamic Route */}
           <Route path="1" element={<Challenge1 />} />
           <Route path="2" element={<Challenge2 />} />
+          {/* //? Dynamic Route */}
           <Route path=":id" element={<OtherChallenges />} />
         </Route>
 
-        {/* * :id can be replaced by another variable (ie :contactId)
+
+
+
+        <Route element={<SharedLayout />}>
+          <Route path="/about" element={<About />} />
+
+          {/* //> Nested route for contact with dynamic route tests
+            * :id can be replaced by another variable (ie :contactId)
             * using :id here also works; even if it is used in challenges Route
             * Use only one :id (params) in Route closure in the same [directory] LEVEL
               - Multiple :ids in the same LEVEL will not result in an error BUT
@@ -34,11 +52,16 @@ const NavRoutes = () => {
               - (:id !== upper/:id) so using path=":id/:id" is acceptable but confusing
                 so using an appropriate name is advised
                 (eg. path=":user/:office" --- /contact/user/office) */}
-        <Route path="contact">
-          <Route index element={<Contact />} />
-          <Route path=":contactId" element={<SomeContact />} />
-          <Route path=":contactId/:contactId" element={<SomeOtherContact />} />
+          <Route path="/contact">
+            <Route index element={<Contact />} />
+            {/* //? Dynamic Routes */}
+            <Route path=":contactId" element={<SomeContact />} />
+            <Route path=":contactId/:contactId" element={<SomeOtherContact />} />
+          </Route>
         </Route>
+
+
+
 
       </Route>
 
@@ -46,4 +69,6 @@ const NavRoutes = () => {
     </Routes>
   )
 }
+
+
 export { NavRoutes }
