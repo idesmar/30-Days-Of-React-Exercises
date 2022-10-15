516f42f8c39ae5f06c641a56e0e7a2973dbc3012
idesmar <idesmar@gmail.com>
2022-10-15 11:25:10 +0800

    done day17
    * Completed mini portfolio with flex div's as projects
    * Used useNavigate and useLocation for dynamic MPNotFound.js

--- day17/src/pages/Exercises/MiniPortfolio/pages/MPAbout.js
+++ day17/src/pages/Exercises/MiniPortfolio/pages/MPAbout.js
@@ -1,6 +1,13 @@
 const MPAbout = () => {
   return (
-    <div>MPAbout</div>
+    <div>
+      <h2>About</h2>
+      {/* cspell: disable */}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt non quia tenetur et veritatis pariatur unde sunt omnis expedita optio?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ab. Corporis qui vel beatae.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus provident enim, facilis dicta at modi, totam, odio adipisci corrupti quis minus voluptatibus fugiat. Dolor laudantium quam sint eum excepturi adipisci dolore aut accusamus voluptates, odio ullam, id numquam maxime nemo?</p>
+      {/* cspell: enable */}
+    </div>
   )
 }
 
--- day17/src/pages/Exercises/MiniPortfolio/pages/MPContact.js
+++ day17/src/pages/Exercises/MiniPortfolio/pages/MPContact.js
@@ -1,6 +1,13 @@
 const MPContact = () => {
   return (
-    <div>MPContact</div>
+    <div>
+      <h2>Contact</h2>
+      {/* cspell: disable */}
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel necessitatibus et exercitationem quam quos!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolor recusandae nulla impedit eveniet atque fugit beatae natus quis! Expedita quam placeat a neque veritatis?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, odit.</p>
+      {/* cspell: enable */}
+    </div>
   )
 }
 
