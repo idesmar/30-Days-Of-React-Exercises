0d19d3008ad10446d68114139b74a20cdcf56d54
idesmar <idesmar@gmail.com>
2022-10-18 02:42:02 +0800

    day18 WIP lev2
    * Axios request coded with AbortController in clean up function
    * Added .small to shared...css
    * Updated label on logs

--- day18/src/services/services.js
+++ day18/src/services/services.js
@@ -1,9 +1,17 @@
+import axios from "axios"
+
 
 const userServices = {
   fetchQna: async (signal) => {
     const URL = '../data/level1qna.json'
     const res = await fetch(URL, { signal: signal })
     return await res.json()
+  },
+
+  axiosCats: async (signal) => {
+    const URL = 'https://api.thecatapi.com/v1/breeds'
+    const res = await axios.get(URL, { signal: signal })
+    return res.data
   },
 }
 
