81c6a0a72f3a9e77543f1c712787ac3f80815dc1
idesmar <idesmar@gmail.com>
2022-10-20 16:43:55 +0800

    day18 Refactored services to be more task specific
    * Broke down services to qnaServices and catServices
    * Currently the only task in each service is fetching BUT ---
    --- it could be a good practice once other HTTP verbs are incorporated

--- day18/src/services/services.js
+++ day18/src/services/services.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 
 
-const userServices = {
+const qnaServices = {
   fetchQna: async (signal) => {
     const URL = '../data/level1qna.json'
     const res = await fetch(URL, { signal: signal })
     return await res.json()
   },
+}
 
+const catServices = {
   axiosCats: async (signal) => {
     const URL = 'https://api.thecatapi.com/v1/breeds'
     const res = await axios.get(URL, { signal: signal })
@@ -16,4 +18,4 @@ const userServices = {
 }
 
 
-export { userServices }
+export { qnaServices, catServices }
