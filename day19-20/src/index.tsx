c2df18408e42152b70f4639c4e7fc51f64f7e6e0
idesmar <idesmar@gmail.com>
2022-10-30 19:26:02 +0800

    day19-20 Top level *.css restructure
    * Renamed styles/ to css-reset/
    * Option to use only reset.css or reset-extended.css
    * Naming convention to these files strictly to *.css
    * Applied *.css restructure to project via index.tsx

--- day19-20/src/index.tsx
+++ day19-20/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { App } from './App/App'
-import './index.module.css'
+import './index.css'
 
 
 const root = ReactDOM.createRoot(
