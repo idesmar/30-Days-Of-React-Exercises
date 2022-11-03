6453edd60fe19797db7faecd9e934fd0c79344f1
idesmar <idesmar@gmail.com>
2022-11-04 05:12:12 +0800

    day19-20 Updated catHelpers
    * Updated so `getAverageFromDelimitedString` can be exported

--- day19-20/src/helpers/catHelpers.ts
+++ day19-20/src/helpers/catHelpers.ts
@@ -1,14 +1,23 @@
-function getCatAverage(arr: Cat[], prop1: keyof Cat, prop2?: keyof Cat['weight']): string {
+
+function getSumFromDelimitedString(str: string) {
+  return str.split(' - ').reduce((acc: number, curr: string) => acc + +curr, 0)
+}
+
+function getAverageFromDelimitedString(str: string) {
+  return (getSumFromDelimitedString(str) / 2).toString()
+}
+
+function getCatsAverage(arr: Cat[], prop1: keyof Cat, prop2?: keyof Cat['weight']): string {
   const total = arr?.map(el => {
-    const subArr = (prop1 === 'weight' && prop2)
+    const valStr = (prop1 === 'weight' && prop2)
       ? el[prop1][prop2]
       : prop1 === 'life_span'
         ? el[prop1]
         : null
-    if (!subArr) return 0
+    if (!valStr) return 0
 
     /* API should provide only 2 values: min & max */
-    return subArr.split(' - ').reduce((acc: number, curr: string) => acc + +curr, 0)
+    return getSumFromDelimitedString(valStr)
   }).reduce((acc, curr) => acc + +curr, 0)
 
   /* `arr.length * 2` is used because each cat has 2 values (min & max) added together */
@@ -20,11 +29,11 @@ function getCatAverage(arr: Cat[], prop1: keyof Cat, prop2?: keyof Cat['weight']
 
 function getCatsGenInfo(cats: Cat[]): CatsGenInfo {
   const count = cats.length.toLocaleString()
-  const aveWeight = getCatAverage(cats, 'weight', 'metric')
-  const aveLifeSpan = getCatAverage(cats, 'life_span')
+  const aveWeight = getCatsAverage(cats, 'weight', 'metric')
+  const aveLifeSpan = getCatsAverage(cats, 'life_span')
 
   return { count, aveWeight, aveLifeSpan }
 }
 
 
-export { getCatAverage, getCatsGenInfo }
+export { getCatsGenInfo, getAverageFromDelimitedString }
